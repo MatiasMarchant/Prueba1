8d69d9f62fae26a4155ec101fbbad92ff9a40675
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-10-13 14:48:07 -0300

    Prueba con cliente

---
+++ cliente.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/MatiasMarchant/Prueba1/tree/master/chat"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Ordencliente{
+		Id:          "Id",
+		Producto:    "Producto",
+		Valor:       15,
+		Tienda:      "Tienda",
+		Destino:     "Destino",
+		Prioritario: true,
+	}
+
+	response, err := c.RedecirOrden(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from Server: %s", response.Producto)
+}
