0b1f4c87e73dbc3371c95dfd7db4f153c83427a1
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-10-14 19:40:08 -0300

    cambios

--- chat/chat.go
+++ chat/chat.go
@@ -9,9 +9,14 @@ import (
 type Server struct {
 }
 
-func (s *Server) RedecirOrden(ctx context.Context, message *Ordencliente) (*Ordencliente, error) {
-	log.Printf("Recibi la orden")
-	log.Printf("")
-	log.Printf("Producto: %s", message.Producto)
+func (s *Server) RedecirOrdenPymes(ctx context.Context, message *Ordenclientepymes) (*Ordenclientepymes, error) {
+	log.Println("Recibi la orden")
+	log.Printf("Producto pymes: %s", message.Producto)
+	return message, nil
+}
+
+func (s *Server) RedecirOrdenRetail(ctx context.Context, message *Ordenclienteretail) (*Ordenclienteretail, error) {
+	log.Println("Recibi la orden retail")
+	log.Printf("Producto retail: %s", message.Producto)
 	return message, nil
 }
--- cliente.go
+++ cliente.go
@@ -1,15 +1,68 @@
 package main
 
 import (
-	"log"
-
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"io"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/MatiasMarchant/Prueba1/tree/master/chat"
 )
 
+func abrircsvpymes() {
+	csvfile, err := os.Open("pymes.csv")
+	if err != nil {
+		log.Fatalln("No pude abrir el csv", err)
+	}
+
+	// Parsear el csvfile
+	r := csv.NewReader(csvfile)
+
+	// Iterar csv
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("1: %s, 2: %s, 3: %s, 4: %s, 5: %s, 6: %s\n", record[0], record[1], record[2], record[3], record[4], record[5])
+	}
+
+}
+
+func preguntasiniciales() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Preguntas iniciales:")
+	fmt.Println("Pymes o Retail?")
+	fmt.Printf("> ")
+	tipotienda, _ := reader.ReadString('\n')
+	tipotienda = strings.Replace(tipotienda, "\n", "", -1)
+	fmt.Println("Respuesta:", tipotienda)
+	fmt.Println("Tiempo en segundos de espera entre el envío de órdenes")
+	fmt.Printf("> ")
+	tiempoespera, _ := reader.ReadString('\n')
+	tiempoespera = strings.Replace(tiempoespera, "\n", "", -1)
+	fmt.Println("Respuesta:", tiempoespera)
+	return tipotienda, tiempoespera
+}
+
 func main() {
+	var tipotienda, tiempoespera string
+	tipotienda, tiempoespera = preguntasiniciales()
+
+	fmt.Println("tipotienda:", tipotienda)
+	fmt.Println("tiempoespera:", tiempoespera)
+
+	//abrircsvpymes()
+
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial("10.6.40.178:9000", grpc.WithInsecure())
 	if err != nil {
@@ -28,7 +81,7 @@ func main() {
 		Prioritario: true,
 	}
 
-	response, err := c.RedecirOrden(context.Background(), &message)
+	response, err := c.RedecirOrdenPymes(context.Background(), &message)
 	if err != nil {
 		log.Fatalf("Error when calling SayHello: %s", err)
 	}
