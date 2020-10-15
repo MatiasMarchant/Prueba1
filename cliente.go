e59fab9d4aa05d146d368161b239fabb692d27a4
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-10-15 16:53:00 -0300

    dist38 server prueba

--- cliente.go
+++ cliente.go
@@ -77,7 +77,7 @@ func main() {
 	fmt.Println("tiempoespera:", tiempoespera)
 
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	conn, err := grpc.Dial("dist38:9000", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %s", err)
 	}
