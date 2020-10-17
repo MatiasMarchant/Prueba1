271b3178e0d500c680d09d5662fba23cd1d8dcfe
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-10-16 21:46:25 -0300

    a

--- camionnormal.go
+++ camionnormal.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -23,21 +24,60 @@ type RegistroCamion struct {
 	origen       string
 	destino      string
 	intentos     string
-	fechaentrega string
+	fechaentrega time.Time
 }
 
-func preguntasiniciales() string {
+func preguntasinicialescamion() (string, string) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Preguntas iniciales")
 	fmt.Println("Tiempo en segundos de espera por segundo paquete")
 	fmt.Printf("> ")
 	tiempoespera, _ := reader.ReadBytes('\n')
-	return string(tiempoespera)
+	fmt.Println("Tiempo en segundos demora envio paquete")
+	fmt.Printf("> ")
+	tiempodemora, _ := reader.ReadBytes('\n')
+	return string(tiempoespera), string(tiempodemora)
+}
+
+//Entregarpaquete es
+func Entregarpaquete(paquete *chat.ColaPaquete, ListaRegistroCamion []RegistroCamion) chat.ColaPaquete {
+	fmt.Println(paquete)
+	paquete.Estado = "En camino"
+	exito := rand.Float64()
+	if exito < 0.8 {
+		// Se entrega paquete
+		// Se modifica registro - fechaentrega
+		for _, elem := range ListaRegistroCamion {
+			if elem.idpaquete == paquete.Idpaquete {
+				elem.fechaentrega = time.Now()
+			}
+		}
+		paquete.Estado = "Recibido"
+		// Se notifica a logistica
+
+	} else {
+		// No se entrega paquete - depende de tipo ver q se hace
+		// Aumentar atributo intentos, etc
+
+		// quizas se notifica a logistica
+
+		// PORMIENTRAS SE DEJA ESTO
+		for _, elem := range ListaRegistroCamion {
+			if elem.idpaquete == paquete.Idpaquete {
+				elem.fechaentrega = time.Now()
+			}
+		}
+		paquete.Estado = "No Recibido"
+
+	}
+	return *paquete
 }
 
 func main() {
-	var tiempoespera string
-	tiempoespera = preguntasiniciales()
+	var ListaRegistroCamion []RegistroCamion
+	var tiempoespera, tiempodemora string
+	tiempoespera, tiempodemora = preguntasinicialescamion()
+	tiempodemoraint, _ := strconv.Atoi(strings.TrimSuffix(tiempodemora, "\r\n")) // CUIDADO LINUX
 	tiempoesperaint, _ := strconv.Atoi(strings.TrimSuffix(tiempoespera, "\r\n")) // CUIDADO LINUX
 
 	var conn *grpc.ClientConn
@@ -61,15 +101,72 @@ func main() {
 		} else { // Si encontró paquete, dormir para esperar el 2do y si no, marchar
 			time.Sleep(time.Second * time.Duration(int64(tiempoesperaint)))
 			paquete2, _ := c.EntregarPaqueteCamionNormal(context.Background(), &idcamion)
-			if paquete2.Idpaquete == "Nopaquetes" {
+			if paquete2.Idpaquete == "Nopaquetes" { // Solo paquete
+
+				// Primero se ingresa a su registro
+				nuevoRegistro := RegistroCamion{
+					idpaquete:    paquete.Idpaquete,
+					tipo:         paquete.Tipo,
+					valor:        paquete.Valor,
+					origen:       paquete.Origen,
+					destino:      paquete.Destino,
+					intentos:     paquete.Intentos,
+					fechaentrega: time.Time{},
+				}
+
+				ListaRegistroCamion = append(ListaRegistroCamion, nuevoRegistro)
+
 				// Marchar solo con paquete
-				paquete.Estado = "En camino"
-				os.Exit(0)
+				*paquete = Entregarpaquete(paquete, ListaRegistroCamion)
+
+				//fmt.Println(paquete)
+				//os.Exit(0)
 			} else {
-				// Marchar con paquete y paquete2
+
+				// Primero se ingresa a su registro
+				nuevoRegistro1 := RegistroCamion{
+					idpaquete:    paquete.Idpaquete,
+					tipo:         paquete.Tipo,
+					valor:        paquete.Valor,
+					origen:       paquete.Origen,
+					destino:      paquete.Destino,
+					intentos:     paquete.Intentos,
+					fechaentrega: time.Time{},
+				}
+
+				nuevoRegistro2 := RegistroCamion{
+					idpaquete:    paquete2.Idpaquete,
+					tipo:         paquete2.Tipo,
+					valor:        paquete2.Valor,
+					origen:       paquete2.Origen,
+					destino:      paquete2.Destino,
+					intentos:     paquete2.Intentos,
+					fechaentrega: time.Time{},
+				}
+
+				ListaRegistroCamion = append(ListaRegistroCamion, nuevoRegistro1)
+				ListaRegistroCamion = append(ListaRegistroCamion, nuevoRegistro2)
+
+				// Marchar con paquete y paquete2 (ver cual es mas caro)
 				paquete.Estado = "En camino"
 				paquete2.Estado = "En camino"
-				os.Exit(0)
+
+				valor1, _ := strconv.Atoi(paquete.Valor)
+				valor2, _ := strconv.Atoi(paquete2.Valor)
+
+				if valor1 > valor2 {
+					// Se entrega paquete primero
+
+					*paquete = Entregarpaquete(paquete, ListaRegistroCamion)
+					*paquete2 = Entregarpaquete(paquete2, ListaRegistroCamion)
+				} else {
+					// Se entrega paquete2 primero
+					*paquete2 = Entregarpaquete(paquete2, ListaRegistroCamion)
+					*paquete = Entregarpaquete(paquete, ListaRegistroCamion)
+
+				}
+
+				//os.Exit(0)
 			}
 		}
 
